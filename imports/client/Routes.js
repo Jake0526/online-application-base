cebfa427134a0b61c36f992141f3ac901ebc4db4
bushidosnap <bushidosnap@gmail.com>
2022-07-19 10:39:23 +0800

    add new routes

--- imports/client/Routes.js
+++ imports/client/Routes.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import onlineApplication from 'meteor/online-application-package';
 
@@ -12,8 +12,15 @@ export default class Routes extends Component {
       <>
       <Router>
         <Switch>
-          <ProtectedRoute path="/" name="dashboard" component={onlineApplication.Dashboard} />
+          {/* <ProtectedRoute path="/" name="dashboard" component={onlineApplication.Dashboard} /> */}
           <ProtectedRoute path="/dashboard1" name="dashboard1" component={onlineApplication.Dashboard1} />
+          <Route path="/job-application" name="job-application" component={onlineApplication.JobApplicationForm} />
+          <Route path="/job-careers" name="job-list" component={onlineApplication.JobList} />
+          <Route path="/profile" name="profile" component={onlineApplication.Profile} />
+          <Route path="/profile-setup/personal-information" name="profile-setup" component={onlineApplication.ProfileSetup} />
+          <Route path="/new/register" name="register" component={onlineApplication.Register} />
+          <Route path="/new/login" name="login" component={onlineApplication.Login} />
+          <Route path="/" name="home-guest" component={onlineApplication.HomeGuest} />
         </Switch>
       </Router>
       </>
