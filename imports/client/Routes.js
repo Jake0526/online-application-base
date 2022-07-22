08fedc1f01abbe1196ab5aaad1020a4c5cb50e93
bushidosnap <bushidosnap@gmail.com>
2022-07-22 09:58:52 +0800

    add route

--- imports/client/Routes.js
+++ imports/client/Routes.js
@@ -14,10 +14,13 @@ export default class Routes extends Component {
         <Switch>
           {/* <ProtectedRoute path="/" name="dashboard" component={onlineApplication.Dashboard} /> */}
           <ProtectedRoute path="/dashboard1" name="dashboard1" component={onlineApplication.Dashboard1} />
-          <Route path="/job-application" name="job-application" component={onlineApplication.JobApplicationForm} />
-          <Route path="/job-careers" name="job-list" component={onlineApplication.JobList} />
+          <Route path="/job-saved" name="job-saved" component={onlineApplication.JobSaved} />
+          <Route path="/job-list" name="job-application-list" component={onlineApplication.JobApplicationList} />
+          <Route path="/job-apply" name="job-application" component={onlineApplication.JobApplicationForm} />
+          <Route path="/job-careers" name="job-search" component={onlineApplication.JobSearch} />
           <Route path="/profile" name="profile" component={onlineApplication.Profile} />
-          <Route path="/profile-setup/personal-information" name="profile-setup" component={onlineApplication.ProfileSetup} />
+          <Route path="/profile-setup" name="profile-setup" component={onlineApplication.ProfileSetup} />
+          <Route path="/recover-password" name="recover-password" component={onlineApplication.ForgotPassword} />
           <Route path="/new/register" name="register" component={onlineApplication.Register} />
           <Route path="/new/login" name="login" component={onlineApplication.Login} />
           <Route path="/" name="home-guest" component={onlineApplication.HomeGuest} />
